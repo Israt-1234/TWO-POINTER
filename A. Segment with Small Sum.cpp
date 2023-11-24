d92f7d7abe3260ddcc0bf83b0a96c5ba7171d703
Israt Jahan <140290748+Israt-1234@users.noreply.github.com>
2023-11-24 13:22:30 +0600

    Codeforces step_2
    largest length segment which sum is at most s
    at most- heigst <=s

---
+++ A. Segment with Small Sum.cpp
@@ -0,0 +1,36 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+//using ll=long long
+#define ll long long
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+const int N=1e5+9;
+int a[N];
+//int d[N][N],pref[N][N];
+int main()
+{
+   // clock_t st= clock();
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n; ll s;
+    cin>>n>>s;
+
+    for(int i=1;i<=n;i++)
+        cin>>a[i];
+      int ans=0;
+    ll sum=0;
+    int  r=1;
+    for(int l=1;l<=n;l++){
+        while(r<=n and sum+a[r]<=s){
+                sum+=a[r];
+            r++;
+        }
+        ans=max(ans,r-l);
+        sum-=a[l];
+    }
+    cout<<ans<<endl;
+    return 0;
+}
+
+
